f7f9dbed742b9f26ba63dcccdda9e7aed5ec6767
Jeff Fu <jefffu233@gmail.com>
2023-08-02 17:18:10 -0700

    random scene

--- main.cpp
+++ main.cpp
@@ -37,33 +37,70 @@ Color ray_color(const Ray &r, const HitTableList &world, int remaining_depth) {
     return (1.0 - t) * Color(1.0, 1.0, 1.0) + t * Color(0.5, 0.7, 1.0);
 }
 
+HitTableList random_scene() {
+    HitTableList world;
+
+    shared_ptr<Material> ground_material = make_shared<Lambertian>(Color(0.5, 0.5, 0.5));
+    world.add(make_shared<Sphere>(Point3(0, -1000, 0), 1000, ground_material));
+
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            double material_selector = rand_double();
+            Point3 center(a + 0.9*rand_double(), 0.2, b + 0.9*rand_double());
+
+            if ((center - Point3(4, 0.2, 0)).length() > 0.9) {
+                shared_ptr<Material> sphere_material;
+
+                // set the material ptr
+                if (material_selector < 0.8) {
+                    // diffuse material
+                    Color albedo = Color::random() * Color::random();
+                    sphere_material = make_shared<Lambertian>(albedo);
+                } else if (material_selector < 0.95) {
+                    // metal
+                    Color albedo = Color::random(0.5, 1);
+                    double fuzz = rand_double(0, 0.5);
+                    sphere_material = make_shared<Metal>(albedo, fuzz);
+                } else {
+                    // dielectric
+                    sphere_material = make_shared<Dielectric>(1.5);
+                }
+
+                // add to scene
+                world.add(make_shared<Sphere>(center, 0.2, sphere_material));
+            }
+        }
+    }
+
+    shared_ptr<Material> material1 = make_shared<Dielectric>(1.5);
+    world.add(make_shared<Sphere>(Point3(0, 1, 0), 1.0, material1));
+
+    shared_ptr<Material> material2 = make_shared<Lambertian>(Color(0.4, 0.2, 0.1));
+    world.add(make_shared<Sphere>(Point3(-4, 1, 0), 1.0, material2));
+
+    shared_ptr<Material> material3 = make_shared<Metal>(Color(0.7, 0.6, 0.5), 0.0);
+    world.add(make_shared<Sphere>(Point3(4, 1, 0), 1.0, material3));
+
+    return world;
+}
+
 int main() {
     // image
-    const double aspect_ratio = 16.0 / 9.0;
-    const int image_width = 400;
+    const double aspect_ratio = 3.0 / 2.0;
+    const int image_width = 1200;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 100;
+    const int samples_per_pixel = 500;
     const int max_reflection_depth = 50;
 
     // world
-    HitTableList world;
-    shared_ptr<Material> material_ground = make_shared<Lambertian>(Color(0.8, 0.8, 0.0));
-    shared_ptr<Material> material_center = make_shared<Lambertian>(Color(0.1, 0.2, 0.5));
-    shared_ptr<Material> material_left = make_shared<Dielectric>(Color(1, 1, 1), 1.5);
-    shared_ptr<Material> material_right = make_shared<Metal>(Color(0.8, 0.6, 0.2), 0.2);
-
-    world.add(make_shared<Sphere>(Point3(0, 0, -1), 0.5, material_center));
-    world.add(make_shared<Sphere>(Point3(0, -100.5, -1), 100, material_ground));
-    world.add(make_shared<Sphere>(Point3(-1, 0, -1), 0.5, material_left));
-    world.add(make_shared<Sphere>(Point3(-1, 0, -1), -0.4, material_left));
-    world.add(make_shared<Sphere>(Point3(1, 0, -1), 0.5, material_right));
+    HitTableList world = random_scene();
 
     // camera
-    Point3 look_from(3, 3, 2);
-    Point3 look_at(0, 0, -1);
+    Point3 look_from(13, 2, 3);
+    Point3 look_at(0, 0, 0);
     Vec3 vup(0, 1, 0);
-    double aperture = 1.5;
-    double focus_dist = (look_at - look_from).length();
+    double aperture = 0.1;
+    double focus_dist = 10;
     Camera camera(look_from, look_at, vup, 20.0, aspect_ratio, aperture, focus_dist);
 
     // ppm header
